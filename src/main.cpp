fb755fc5fbf77bbc4249f272acd576b00f9e4c0d
Girish G <girishgg6363@gmail.com>
2025-12-31 22:16:05 +0530

    last commit of 2025 & removed unused stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,3 @@
-// #include "lexer/tokenizer.cpp"
-// #include <fstream>
-// #include <sstream>
-// #include <iostream>
-
-// int main(int argc, char** argv) {
-//     if (argc < 2) {
-//         std::cerr << "Usage: " << argv[0] << " <filename>\n";
-//         return 1;
-//     }
-
-//     std::ifstream file(argv[1]);
-//     if (!file.is_open()) {
-//         std::cerr << "Failed to open file: " << argv[1] << "\n";
-//         return 1;
-//     }
-
-//     std::stringstream buffer;
-//     buffer << file.rdbuf();
-//     std::string source = buffer.str();
-
-//     std::cout << "Source: " << source << "\n"; // ✅ TEMP DEBUG: See if file is read
-
-//     Tokenizer tokenizer(source);
-//     std::vector<Token> tokens = tokenizer.tokenize();
-
-//     for (const Token& token : tokens) {
-//         std::cout << "Token: " << (int)token.type << ", Value: " << token.value << ", Line: " << token.line << "\n";
-//     }
-
-//     return 0;
-// }
-
-
 #include "lexer/tokenizer.cpp"
 #include "parser/parser.hpp"
 #include "parser/ast.hpp"
